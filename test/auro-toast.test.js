b4c50c3c03489af1612b0900b2036ccc66b29f6b
Dale Sande <dale.sande@gmail.com>
2020-11-20 12:41:49 -0800

    initial commit

---
+++ test/auro-toast.test.js
@@ -0,0 +1,28 @@
+import { fixture, html, expect } from '@open-wc/testing';
+import sinon from 'sinon';
+import '../src/auro-toast.js';
+
+describe('auro-toast', () => {
+  it('sets the CSS class on auro-toast > div element', async () => {
+    const el = await fixture(html`
+      <auro-toast cssclass="testClass"></auro-toast>
+    `);
+
+    const div = el.shadowRoot.querySelector('div');
+    expect(div.className).to.equal('testClass');
+  });
+
+  it('auro-toast is accessible', async () => {
+    const el = await fixture(html`
+      <auro-toast cssclass="testClass"></auro-toast>
+    `);
+
+    await expect(el).to.be.accessible();
+  });
+
+  it('auro-toast custom element is defined', async () => {
+    const el = await !!customElements.get("auro-toast");
+
+    await expect(el).to.be.true;
+  });
+});
